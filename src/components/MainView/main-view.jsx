3f944176fa456a2f91bf2955845428fed600d964
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-05-01 21:54:54 +0200

    main-view component logic added

---
+++ src/components/MainView/main-view.jsx
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import { useState } from "react";
+import { MovieCard } from "../MovieCard/movie-card";
+import { MovieView } from "../MovieView/movie-view";
+
+export const MainView = () => {
+  const [movies, setMovies] = useState([
+    {
+      id: 1,
+      title: "The Shawshank Redemption",
+      description:
+        "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+      genre: "Drama",
+      image:
+        "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
+      director: "Frank Darabont",
+    },
+    {
+      id: 2,
+      title: "Inception",
+      description:
+        "A thief who enters the dreams of others to steal secrets from their subconscious. His latest target is a corporate heir who must implant an idea into his mind.",
+      genre: "Science Fiction",
+      image: "https://www.movieposter.com/posters/archive/main/101/MPW-50948",
+      director: "Christopher Nolan",
+    },
+    {
+      id: 3,
+      title: "The Godfather",
+      description:
+        "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
+      genre: "Crime",
+      image: "https://www.movieposter.com/posters/archive/main/10/A70-4915",
+      director: "Francis Ford Coppola",
+    },
+  ]);
+
+  if (movies.length === 0) {
+    return <div>The list is empty!</div>;
+  }
+
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  if (selectedMovie) {
+    return (
+      <MovieView
+        movie={selectedMovie}
+        onBackClick={() => setSelectedMovie(null)}
+      />
+    );
+  }
+
+  return (
+    <div>
+      {movies.map((movie) => (
+        <MovieCard
+          key={movie.id}
+          movie={movie}
+          onMovieClick={(newSelectedMovie) => {
+            setSelectedMovie(newSelectedMovie);
+          }}
+        />
+      ))}
+    </div>
+  );
+};
