dc1717747e62fed8d37c340763b73ba0f96dc520
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-05-03 15:51:33 +0200

    fixed data replaced with api request

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -2,46 +2,37 @@ import React, { useState } from "react";
 import { useState } from "react";
 import { MovieCard } from "../MovieCard/movie-card";
 import { MovieView } from "../MovieView/movie-view";
+import { useState, useEffect } from "react";
 
 export const MainView = () => {
-  const [movies, setMovies] = useState([
-    {
-      id: 1,
-      title: "The Shawshank Redemption",
-      description:
-        "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-      genre: "Drama",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/8/81/ShawshankRedemptionMoviePoster.jpg",
-      director: "Frank Darabont",
-    },
-    {
-      id: 2,
-      title: "Inception",
-      description:
-        "A thief who enters the dreams of others to steal secrets from their subconscious. His latest target is a corporate heir who must implant an idea into his mind.",
-      genre: "Science Fiction",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/2/2e/Inception_%282010%29_theatrical_poster.jpg",
-      director: "Christopher Nolan",
-    },
-    {
-      id: 3,
-      title: "The Godfather",
-      description:
-        "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son.",
-      genre: "Crime",
-      image:
-        "https://upload.wikimedia.org/wikipedia/en/1/1c/Godfather_ver1.jpg",
-      director: "Francis Ford Coppola",
-    },
-  ]);
+  const [movies, setMovies] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
+
+  useEffect(() => {
+    fetch("https://movies-api-ms-b2173cbfa01b.herokuapp.com/movies")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("movies from api: ", data);
+        const moviesFromApi = data.map((doc) => {
+          return {
+            id: doc._id,
+            title: doc.Title,
+            description: doc.Description,
+            genre: doc.Genre.Name,
+            genreDescription: doc.Genre.Description,
+            director: doc.Director.Name,
+            image: doc.ImagePath,
+          };
+        });
+
+        console.log("movies after map:", moviesFromApi);
+        setMovies(moviesFromApi);
+      });
+  }, []);
 
   if (movies.length === 0) {
     return <div>The list is empty!</div>;
   }
-
-  const [selectedMovie, setSelectedMovie] = useState(null);
 
   if (selectedMovie) {
     return (
@@ -54,6 +45,8 @@ export const MainView = () => {
 
   return (
     <div>
+      <pre>{JSON.stringify(selectedMovie, undefined, 2)}</pre>
+
       {movies.map((movie) => (
         <MovieCard
           key={movie.id}
