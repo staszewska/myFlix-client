b3f99cb3f500aa51cb6d6fdd75b594c9c825c642
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-05-07 20:42:49 +0200

    width added to MovieCard

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -4,7 +4,7 @@ import { Button, Card } from "react-bootstrap";
 export const MovieCard = ({ movie, onMovieClick }) => {
   return (
     <Card className="h-100">
-      <Card.Img variant="top" src={movie.image} />
+      <Card.Img className="w-100" variant="top" src={movie.image} />
       <Card.Body>
         <Card.Title>{movie.title}</Card.Title>
         <Card.Text>{movie.director}</Card.Text>
