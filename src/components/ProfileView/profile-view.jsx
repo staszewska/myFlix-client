e7cf728184397e525b32462255733683ea5f7765
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-05-10 14:11:15 +0200

    user information in ProfileView generated

--- src/components/ProfileView/profile-view.jsx
+++ src/components/ProfileView/profile-view.jsx
@@ -1,15 +1,27 @@
 import React, { useState, useEffect } from "react";
 import { Container, Form, Button } from "react-bootstrap";
+import { Navigate } from "react-router-dom";
 
-const ProfileView = () => {
-  const [user, setUser] = useState({
-    Name: "",
-    Password: "",
-    Email: "",
-    Birthday: newDate(),
-    Country: "",
-    Gender: "",
-  });
+export const ProfileView = ({ user }) => {
+  console.log(user);
+  const token = localStorage.getItem("token");
 
-  return <h1>Profile</h1>;
+  if (!user) {
+    return (
+      <>
+        <Navigate to="/login" />
+      </>
+    );
+  }
+
+  return (
+    <Container>
+      <h1>Profile</h1>
+      <p>Username: {user.Name}</p>
+      <p>Email: {user.Email}</p>
+      <p>Country: {user.Country}</p>
+
+      <h2>Favourite Movies:</h2>
+    </Container>
+  );
 };
