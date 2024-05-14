a3a83014b4faf4e3dcef865c0f36ccd62ca3edf4
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-05-14 16:54:46 +0200

    bootstrap added to movie filter

--- src/components/Filter/genre-filter.jsx
+++ src/components/Filter/genre-filter.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Dropdown from "react-bootstrap/Dropdown";
 
 export const GenreFilter = ({ onSelectGenre }) => {
   const genres = [
@@ -16,16 +17,33 @@ export const GenreFilter = ({ onSelectGenre }) => {
 
   return (
     <div>
-      <label htmlFor="genre">Select Genre:</label>
       {/* event handler triggered when the value of dropdown changes  */}
-      <select id="genre" onChange={(e) => onSelectGenre(e.target.value)}>
-        <option value="">All Genres</option>
-        {genres.map((genre) => (
-          <option key={genre} value={genre}>
-            {genre}
-          </option>
-        ))}
-      </select>
+      <Dropdown onSelect={(eventKey) => onSelectGenre(eventKey)}>
+        <Dropdown.Toggle variant="info" id="genre-dropdown">
+          All Genres
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item eventKey="">All Genres</Dropdown.Item>
+          {genres.map((genre) => (
+            <Dropdown.Item key={genre} eventKey={genre}>
+              {genre}
+            </Dropdown.Item>
+          ))}
+        </Dropdown.Menu>
+      </Dropdown>
     </div>
+    // <div>
+    //   <label htmlFor="genre">Select Genre:</label>
+    //   {/* event handler triggered when the value of dropdown changes  */}
+    //   <select id="genre" onChange={(e) => onSelectGenre(e.target.value)}>
+    //     <option value="">All Genres</option>
+    //     {genres.map((genre) => (
+    //       <option key={genre} value={genre}>
+    //         {genre}
+    //       </option>
+    //     ))}
+    //   </select>
+    // </div>
   );
 };
