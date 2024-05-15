d54463b88d899a735994c9ce22fcbd527bb74cd4
Malgorzata Staszewska-Bui <136642341+staszewska@users.noreply.github.com>
2024-05-15 16:23:25 +0200

    filtering out based on genre

--- src/components/Filter/genre-filter.jsx
+++ src/components/Filter/genre-filter.jsx
@@ -1,19 +1,36 @@
 import React from "react";
 import Dropdown from "react-bootstrap/Dropdown";
+import { useState, useEffect } from "react";
+import { useParams } from "react-router";
 
 export const GenreFilter = ({ onSelectGenre }) => {
-  const genres = [
-    "Action",
-    "Adventure",
-    "Biography",
-    "Romance",
-    "Crime",
-    "Comedy",
-    "War",
-    "Drama",
-    "Sci-Fi",
-    "Thriller",
-  ];
+  const [genres, setGenres] = useState([]);
+  // const { genre } = useParams();
+  const token = localStorage.getItem("token");
+
+  useEffect(() => {
+    fetch(`https://movies-api-ms-b2173cbfa01b.herokuapp.com/movies/`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data);
+
+        // const extractedGenres = data.map((movie) => movie.Genre.Name);
+        //Set is created and filters out duplicates
+        const extractedGenres = Array.from(
+          new Set(data.map((movie) => movie.Genre.Name))
+        );
+
+        setGenres(extractedGenres);
+        console.log(extractedGenres);
+      })
+      .catch((error) => console.error("Error fetching genres:", error));
+  }, []);
 
   return (
     <div>
